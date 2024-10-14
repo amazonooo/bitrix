8845db77f609061632ba13eee3c362aa234f3bfa
amazonooo <garikogannisyan.9@gmail.com>
2024-10-14 23:06:00 +0300

    added timeline

--- src/app/implementation/components/Settings.tsx
+++ src/app/implementation/components/Settings.tsx
@@ -16,7 +16,7 @@ export default function Settings() {
 						className='py-10 flex flex-col lg:flex-row gap-x-40 gap-y-5 border-b-2 border-use-gray'
 					>
 						<div className='flex items-center gap-x-2.5'>
-							<div className='lg:w-2.5 lg:h-2.5 w-3 h-3 bg-primary-blue rounded-[1px]' />
+							<div className='lg:w-2.5 lg:h-2.5 w-3 h-3 bg-primary-blue rounded-[1px] hidden lg:block' />
 							<h2 className='text-xl lg:text-[26px] font-semibold min-w-[425px]'>
 								{sd.name}
 							</h2>
--- src/app/implementation/components/Stages.tsx
+++ src/app/implementation/components/Stages.tsx
@@ -1,17 +1,10 @@
 import Title from '@/components/ui/Title'
-import { stagesData } from '@/data/stagesData'
 
 export default function Stages() {
-  return (
+	return (
 		<section>
 			<Title className='mb-20 max-w-[780px]' title='Этапы внедрения Bitrix24' />
-
-			<div className='w-full h-[5px] relative bg-primary-blue ml-24'>
-				<div className='absolute w-[70px] h-[70px] flex items-center justify-center rounded-full bg-primary-blue text-[2.5rem] font-bold text-white -left-8 top-1/2 -translate-y-1/2'>
-					1
-				</div>
-				<h2 className='text-[2rem] leading-[38px] text-center font-medium absolute max-w-[268px] top-14 -left-[105px]'>Оценка бизнес-процессов</h2>
-			</div>
+			
 		</section>
 	)
 }
