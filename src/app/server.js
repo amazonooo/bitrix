0886ae4b6216c03efdc78656a5d58042a70505ca
amazonooo <garikogannisyan.9@gmail.com>
2024-10-20 00:05:28 +0300

    changes

--- src/app/server.js
+++ src/app/server.js
@@ -1,5 +1,4 @@
-const { createServer } = require('http') // Меняем https на http
-const { parse } = require('url')
+const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,11 +6,16 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-	createServer((req, res) => {
-		const parsedUrl = parse(req.url, true)
-		handle(req, res, parsedUrl)
-	}).listen(3000, err => {
+	const server = express()
+
+	// Обработка всех запросов через Next.js
+	server.all('*', (req, res) => {
+		return handle(req, res)
+	})
+
+	const port = process.env.PORT || 3000
+	server.listen(port, err => {
 		if (err) throw err
-		console.log('> Server is running on http://localhost:3000')
+		console.log(`> Ready on http://localhost:${port}`)
 	})
 })
